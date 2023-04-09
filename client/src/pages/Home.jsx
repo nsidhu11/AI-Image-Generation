453cd0959ed502f27951fa30cf09191e9d292736
Navpreet Singh Sidhu <73638005+nsidhu11@users.noreply.github.com>
2023-04-09 16:59:33 -0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("https://ai-tools-6onu.onrender.com/api/v1/post, {
+        const response = await fetch("https://ai-tools-6onu.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
