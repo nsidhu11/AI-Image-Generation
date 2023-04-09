6ab61dbda52fa81beba93f2b63378cc26ec9a981
Navpreet Singh Sidhu <73638005+nsidhu11@users.noreply.github.com>
2023-04-09 16:57:20 -0700

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("https://ai-tools-6onu.onrender.com/api/v1/dalle", {
+        const response = await fetch("https://ai-tools-6onu.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
